27d1c0775c14039abbe9b112a4e183b13749035f
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:17:53 +0100

    lesson 10

--- objects.js
+++ objects.js
@@ -2,10 +2,17 @@ function User(email, name){
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function(){
-        console.log(this.email, 'has logged in');
-    };
 }
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
+};
+
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, 'has logged out');
+};
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
