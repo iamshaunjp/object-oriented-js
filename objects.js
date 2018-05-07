efde1c4a53eeac30768dd5d7c0217b4819eaaf31
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 20:56:42 +0100

    lesson-2

---
+++ objects.js
@@ -0,0 +1,13 @@
+var userOne = {
+    email: 'ryu@ninjas.com',
+    name: 'Ryu',
+    login(){
+        console.log(this.email, 'has logged in');
+    },
+    logout(){
+        console.log(this.email, 'has logged out');
+    }
+};
+
+userOne.login();
+userOne.logout();
