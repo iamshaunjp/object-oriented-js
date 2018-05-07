2ec62375de3b973f4945112583a1a75ce26a44f8
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:19:13 +0100

    lesson 11

--- objects.js
+++ objects.js
@@ -14,8 +14,20 @@ User.prototype.logout = function(){
     console.log(this.email, 'has logged out');
 };
 
+function Admin(...args){
+    User.apply(this, args);
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    });
+};
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
