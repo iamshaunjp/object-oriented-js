b82b5cc81f69067c9d525bf0da9ae6efffc24c32
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:11:26 +0100

    lesson 6

--- objects.js
+++ objects.js
@@ -1,3 +1,18 @@
 class User {
-    
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+    }
 }
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+
+userOne.login();
+userTwo.logout();
