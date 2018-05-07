fc67174ce6e3af5afb33b3dea980c30422cc83dc
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:09:21 +0100

    lesson 4

--- objects.js
+++ objects.js
@@ -1,27 +1,3 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
-
-var prop = 'name'
-
-// userOne.email = 'ryu2k18@ninjas.com';
-// userOne['email'] = 'ryu2k18@ninjas.com';
-// userOne[prop] = 'Ryuke';
-
-// console.log(userOne);
-
-userOne.age = 25;
-userOne.logInfo = function(){
-    console.log(this.name, this.age, this.email);
-};
-
-userOne.logInfo();
-userOne.login();
-userOne.logout();
+class User {
+    
+}
