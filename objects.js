bac3431b0441c02d7e6763441434ff1d410f9d8b
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:15:43 +0100

    lesson 9

--- objects.js
+++ objects.js
@@ -1,39 +1,14 @@
-class User {
-    constructor(email, name){
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login(){
-        console.log(this.email, 'just logged in');
-        return this;
-    }
-    logout(){
-        console.log(this.email, 'just logged out');
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, 'score is now', this.score);
-        return this;
-    }
-}
-
-class Admin extends User {
-    deleteUser(user){
-        users = users.filter(u => {
-            return u.email != user.email
-        });
-    }
+function User(email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
+    };
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-var users = [userOne, userTwo, admin];
-
-// admin.deleteUser(userTwo);
-userTwo.deleteUser(userOne); // won't work
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
