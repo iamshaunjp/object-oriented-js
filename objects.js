7c0397f50c53584aa2a59dae132c53a8c7cfc4a7
Shaun <shaunjp@shaun-pelling.com>
2018-05-07 21:14:25 +0100

    lesson 8

--- objects.js
+++ objects.js
@@ -19,9 +19,21 @@ class User {
     }
 }
 
+class Admin extends User {
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email
+        });
+    }
+}
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-userOne.login().updateScore().updateScore().logout();
+var users = [userOne, userTwo, admin];
 
-console.log(userOne);
+// admin.deleteUser(userTwo);
+userTwo.deleteUser(userOne); // won't work
+
+console.log(users);
